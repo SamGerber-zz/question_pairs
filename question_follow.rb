84f41a2addd1a3f3a310778edeea9634a2b89277
SamGerber <gerbyxvi@gmail.com>
2016-01-19 13:53:59 -0800

    Implemented Medium (basic join stuff)

--- question_follow.rb
+++ question_follow.rb
@@ -20,4 +20,32 @@ class QuestionFollow
     return nil if data.empty?
     self.new(data.first)
   end
+
+  def self.followers_for_question_id( question_id )
+    data = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+      SELECT
+        users.id, users.fname, users.lname
+      FROM
+        users
+      JOIN question_follows ON users.id = question_follows.user_id
+      WHERE
+        question_follows.question_id = ?
+    SQL
+
+    data.map { |user| User.new(user) }
+  end
+
+  def self.followed_questions_for_user_id( user_id )
+    data = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+      SELECT
+        questions.id, questions.title, questions.body, questions.author_id
+      FROM
+        questions
+      JOIN question_follows ON questions.id = question_follows.question_id
+      WHERE
+        question_follows.user_id = ?
+    SQL
+
+    data.map { |question| Question.new(question) }
+  end
 end
